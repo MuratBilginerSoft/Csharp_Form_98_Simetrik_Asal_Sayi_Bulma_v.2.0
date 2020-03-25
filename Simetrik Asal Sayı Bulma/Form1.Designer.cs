939d98b4d4a49fce6820935b9c04f207cb5ddcd8
Murat Bilginer <muratbilginer09@gmail.com>
2020-03-25 23:32:48 +0300

    Initial commit

---
+++ Simetrik Asal Sayı Bulma/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Simetrik_Asal_Sayı_Bulma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(16, 18);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(288, 112);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Simetrik Asal Sayı Bul";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = global::Simetrik_Asal_Sayı_Bulma.Properties.Resources._4;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(15, 65);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(261, 37);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "BUL";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1 Basamaklı",
+            "2 Basamaklı",
+            "3 Basamaklı",
+            "4 Basamaklı",
+            "5 Basamaklı"});
+            this.comboBox1.Location = new System.Drawing.Point(133, 27);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(143, 27);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(11, 30);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 18);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Basamak Sayısı";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listBox1);
+            this.groupBox2.Location = new System.Drawing.Point(16, 138);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Size = new System.Drawing.Size(288, 327);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Değerler";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 19;
+            this.listBox1.Location = new System.Drawing.Point(15, 28);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(261, 289);
+            this.listBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(321, 473);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MB Simetrik Asal Sayı v2.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Simetrik Asal Sayı Bulma/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Simetrik_Asal_Sayı_Bulma
+{
+    public partial class Form1 : Form
+    {
+        string simetrik1,simetrik2="";
+        int simetrik;
+        int[] a1 = new int[5000];
+        int[] a2 = new int[5000];
+        int[] a3 = new int[5000];
+        int[] a4 = new int[5000];
+        int[] a5 = new int[5000];
+        string[] z1 = new string[5];
+        int c1 = 1, c2 = 0, c3 = 0, c4 = 0, c5 = 0;
+        
+
+        public void asalbulma()
+        {
+            for (int j = 3; j < 100000; j++)
+            {
+                int a = 0;
+
+                for (int i = 2; i < j; i++)
+                {
+                    if (j % i == 0)
+                    {
+                        a++;
+                        break;
+                    }
+                }
+
+                if (a == 0)
+                {
+                    simetrik1 = j.ToString();
+                    simetrik2 = "";
+
+                    int y1 = 0;
+
+                    foreach (char t in simetrik1)
+                    {
+                        z1[y1] = t.ToString();
+                        y1++;
+                    }
+
+                    int r = z1.Length-1;
+
+                    for (int i = r; i >=0; i--)
+                    {
+                        simetrik2 += z1[i];
+                    }
+                    
+                     simetrik = int.Parse(simetrik2);
+
+                     int b = 0;
+
+                     for (int i = 2; i < simetrik; i++)
+                     {
+
+                         if (simetrik % i == 0)
+                         {
+                             b++;
+                             break;
+                         }
+                     }
+
+                     if (b == 0)
+                     {
+                         if (simetrik1.Length == 1)
+                         {
+                             a1[c1] = j;
+                             c1++;
+                         }
+
+                         else if (simetrik1.Length == 2)
+                         {
+                             a2[c2] = j;
+                             c2++;
+                         }
+
+                         else if (simetrik1.Length == 3)
+                         {
+                             a3[c3] = j;
+                             c3++;
+                         }
+                         else if (simetrik1.Length == 4)
+                         {
+                             a4[c4] = j;
+                             c4++;
+                         }
+                         else if (simetrik1.Length == 5)
+                         {
+                             a5[c5] = j;
+                             c5++;
+                         }
+
+
+                     }
+
+                }
+
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            
+            listBox1.Items.Clear();
+
+            if (comboBox1.Text == "1 Basamaklı")
+            {
+                for (int i = 0; i < c1; i++)
+                {
+                    listBox1.Items.Add(a1[i]);
+                }
+            }
+
+            else if (comboBox1.Text == "2 Basamaklı")
+            {
+                for (int i = 0; i < c2; i++)
+                {
+                    listBox1.Items.Add(a2[i]);
+                }
+            }
+
+            else if (comboBox1.Text == "3 Basamaklı")
+            {
+                for (int i = 0; i < c3; i++)
+                {
+                    listBox1.Items.Add(a3[i]);
+                }
+            }
+
+            else if (comboBox1.Text == "4 Basamaklı")
+            {
+                for (int i = 0; i < c4; i++)
+                {
+                    listBox1.Items.Add(a4[i]);
+                }
+            }
+
+            else if (comboBox1.Text == "5 Basamaklı")
+            {
+                for (int i = 0; i < c5 - 1; i++)
+                {
+                    listBox1.Items.Add(a5[i]);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            asalbulma();
+            a1[0] = 2;
+        }
+    }
+}
